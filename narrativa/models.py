d5921e5e6276e9f385da29916adbd0a14fa4d285
cristersom <cristersom@gmail.com>
2025-08-22 17:26:41 -0300

    Settings app

--- narrativa/models.py
+++ narrativa/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+LISTA_CATEGORIAS = (
+    ("BOASVINDAS", "Boas vindas"),
+    ("TRATAMENTO", "Tratamento"),
+    ("REVISÃO", "Revisão"),
+    ("ENCERRAMENTO", "Encerramento"),
+    ("OUTROS", "Outros"),
+)
+class Narrativa(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumb_narrativas')
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
