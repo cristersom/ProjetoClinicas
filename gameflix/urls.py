5bc635c71ac9183086b6c23726cbee5c3d0ea5da
cristersom <cristersom@gmail.com>
2025-08-25 17:53:14 -0300

    Static e Media

--- gameflix/urls.py
+++ gameflix/urls.py
@@ -1,3 +1,5 @@
+
+
 """
 URL configuration for gameflix project.
 
@@ -16,9 +18,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('/', include('narrativa.urls'))
 
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
