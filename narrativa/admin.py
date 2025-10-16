d4bb9d9ad68c2d7d46889480386289457d0ad814
cristersom <cristersom@gmail.com>
2025-10-16 19:16:42 -0300

    Corrige IndentationError no admin.py e finaliza nested admin

--- narrativa/admin.py
+++ narrativa/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-import nested_admin # <-- 1. Importamos a nova biblioteca
+import nested_admin
 from .models import (
     Narrativa, Cena, Escolha, Questionario, Pergunta, Usuario, Resposta,
     SessaoPaciente, OpcaoResposta
@@ -8,36 +8,52 @@ from .models import (
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
 
-# ... (As classes RespostaResource e NarrativaPerfilFilter continuam iguais) ...
-class RespostaResource(resources.ModelResource): #...
-class NarrativaPerfilFilter(admin.SimpleListFilter): #...
+# --- Classes de customização (Exportação e Filtro) ---
+class RespostaResource(resources.ModelResource):
+    questionario = resources.Field(attribute='pergunta__questionario__titulo', column_name='Questionário')
+    perfil_narrativa = resources.Field(column_name='Perfil (Narrativa)')
+    class Meta:
+        model = Resposta
+        fields = ('id', 'session_key', 'perfil_narrativa', 'questionario', 'pergunta__texto_pergunta', 'texto_resposta', 'data_resposta')
+        export_order = fields
+    def dehydrate_perfil_narrativa(self, resposta):
+        try:
+            sessao = SessaoPaciente.objects.get(session_key=resposta.session_key)
+            if sessao.narrativa_perfil:
+                return sessao.narrativa_perfil.titulo
+        except SessaoPaciente.DoesNotExist:
+            return 'Não definido'
+        return 'Não definido'
+
+class NarrativaPerfilFilter(admin.SimpleListFilter):
+    title = 'por Perfil (Narrativa)'
+    parameter_name = 'narrativa_perfil'
+    def lookups(self, request, model_admin):
+        return [(narrativa.id, narrativa.titulo) for narrativa in Narrativa.objects.all()]
+    def queryset(self, request, queryset):
+        if self.value():
+            sessoes = SessaoPaciente.objects.filter(narrativa_perfil_id=self.value())
+            lista_de_session_keys = sessoes.values_list('session_key', flat=True)
+            return queryset.filter(session_key__in=lista_de_session_keys)
+        return queryset
 
 # --- Classes Inline para edição ---
-
 class EscolhaInline(admin.TabularInline):
     model = Escolha
     fk_name = 'cena_origem'
     extra = 1
 
-# --- MUDANÇAS IMPORTANTES AQUI ---
-# O OpcaoRespostaInline agora herda do nested_admin
 class OpcaoRespostaInline(nested_admin.NestedTabularInline):
     model = OpcaoResposta
     extra = 1
 
-# O PerguntaInline agora herda do nested_admin e pode ter seus próprios inlines
 class PerguntaInline(nested_admin.NestedTabularInline):
     model = Pergunta
     fk_name = 'questionario'
     extra = 1
-    # A mágica acontece aqui: aninhamos o inline de opções DENTRO do inline de perguntas
     inlines = [OpcaoRespostaInline]
 
 # --- Registros dos Modelos no Admin ---
-
-# O PerguntaAdmin não é mais necessário, pois tudo é feito na tela do Questionário
-# @admin.register(Pergunta) ... foi removido.
-
 @admin.register(Cena)
 class CenaAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'narrativa')
@@ -49,16 +65,35 @@ class NarrativaAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'categoria', 'data_criacao', 'cena_inicial')
     list_filter = ('categoria',)
 
-# O QuestionarioAdmin agora herda do nested_admin
 @admin.register(Questionario)
 class QuestionarioAdmin(nested_admin.NestedModelAdmin):
     list_display = ('titulo', 'cena_associada')
     inlines = [PerguntaInline]
 
-# ... (O resto do seu admin.py com SessaoPacienteAdmin, RespostaAdmin, etc. continua igual) ...
 @admin.register(SessaoPaciente)
-class SessaoPacienteAdmin(admin.ModelAdmin): #...
+class SessaoPacienteAdmin(admin.ModelAdmin):
+    list_display = ('session_key_abreviada', 'narrativa_perfil', 'data_criacao')
+    list_filter = ('narrativa_perfil',)
+    readonly_fields = ('session_key', 'narrativa_perfil', 'data_criacao')
+    def session_key_abreviada(self, obj):
+        return obj.session_key[:8] + '...'
+    session_key_abreviada.short_description = 'Sessão do Paciente'
+
 @admin.register(Resposta)
-class RespostaAdmin(ImportExportModelAdmin): #...
+class RespostaAdmin(ImportExportModelAdmin):
+    resource_class = RespostaResource
+    list_display = ('id', 'questionario_associado', 'pergunta', 'session_key_abreviada', 'texto_resposta', 'data_resposta')
+    list_filter = (NarrativaPerfilFilter, 'pergunta__questionario', 'data_resposta',)
+    search_fields = ('texto_resposta', 'session_key')
+    ordering = ('session_key', 'pergunta__questionario', 'pergunta__id')
+
+    def questionario_associado(self, obj):
+        return obj.pergunta.questionario.titulo
+    questionario_associado.short_description = 'Questionário'
+
+    def session_key_abreviada(self, obj):
+        return obj.session_key[:8] + '...'
+    session_key_abreviada.short_description = 'Sessão do Paciente'
 
 admin.site.register(Usuario, UserAdmin)
+# O PerguntaAdmin não é mais registrado separadamente, pois agora é gerenciado pelo nested_admin
