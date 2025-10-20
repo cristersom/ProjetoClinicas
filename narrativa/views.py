c00595d382a3abe5bf12d46d42691fda72ea5117
cristersom <cristersom@gmail.com>
2025-10-20 12:01:24 -0300

    Remove registro duplicado do CenaAdmin de views.py

--- narrativa/views.py
+++ narrativa/views.py
@@ -5,6 +5,7 @@ from .forms import CriarContaForm, FormHomepage
 from django.views.generic import ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+# --- IMPORTANTE: NÃO HÁ MAIS @admin.register(Cena) AQUI ---
 
 class Homepage(FormView):
     template_name = "homepage.html"
@@ -14,162 +15,157 @@ class Homepage(FormView):
         if request.user.is_authenticated:
             return redirect('narrativa:narrativas')
         else:
-            return super().get(request, *args, **kwargs)
+            return super().get(request, *args, **kwargs) #
 
     def get_success_url(self):
-        email = self.request.POST.get("email")
-        usuarios = Usuario.objects.filter(email=email)
+        email = self.request.POST.get("email") #
+        usuarios = Usuario.objects.filter(email=email) #
         if usuarios:
-            return reverse('narrativa:login')
+            return reverse('narrativa:login') #
         else:
-            return reverse('narrativa:criarconta')
+            return reverse('narrativa:criarconta') #
 
 
 class Narrativas(LoginRequiredMixin, ListView):
-    template_name = "narrativas.html"
-    model = Narrativa
+    template_name = "narrativas.html" #
+    model = Narrativa #
 
 
 class Detalhes(LoginRequiredMixin, DetailView):
-    template_name = "detalhes.html"
-    model = Narrativa
+    template_name = "detalhes.html" #
+    model = Narrativa #
 
     def get(self, request, *args, **kwargs):
-        narrativa = self.get_object()
-        narrativa.visualizacoes += 1
-        narrativa.save()
-        usuario = request.user
-        usuario.narrativas_vistas.add(narrativa)
-        return super().get(request, *args, **kwargs)
+        narrativa = self.get_object() #
+        narrativa.visualizacoes += 1 #
+        narrativa.save() #
+        usuario = request.user #
+        usuario.narrativas_vistas.add(narrativa) #
+        return super().get(request, *args, **kwargs) #
 
     def get_context_data(self, **kwargs):
-        context = super(Detalhes, self).get_context_data(**kwargs)
-        relacionados = Narrativa.objects.filter(categoria=self.get_object().categoria).order_by('-visualizacoes')[0:5]
-        context["relacionados"] = relacionados
-        return context
+        context = super(Detalhes, self).get_context_data(**kwargs) #
+        relacionados = Narrativa.objects.filter(categoria=self.get_object().categoria).order_by('-visualizacoes')[0:5] #
+        context["relacionados"] = relacionados #
+        return context #
 
 
 class Pesquisa(LoginRequiredMixin, ListView):
-    template_name = "pesquisa.html"
-    model = Narrativa
+    template_name = "pesquisa.html" #
+    model = Narrativa #
 
     def get_queryset(self):
-        termo_pesquisa = self.request.GET.get('query')
+        termo_pesquisa = self.request.GET.get('query') #
         if termo_pesquisa:
-            object_list = self.model.objects.filter(titulo__icontains=termo_pesquisa)
-            return object_list
+            object_list = self.model.objects.filter(titulo__icontains=termo_pesquisa) #
+            return object_list #
         else:
-            return None
+            return None #
 
 
 class PerfilView(LoginRequiredMixin, UpdateView):
-    template_name = "perfil.html"
-    model = Usuario
-    fields = ['first_name', 'last_name', 'email']
+    template_name = "perfil.html" #
+    model = Usuario #
+    fields = ['first_name', 'last_name', 'email'] #
 
     def get_success_url(self):
-        return reverse('narrativa:narrativas')
+        return reverse('narrativa:narrativas') #
 
 
 class Criarconta(FormView):
-    template_name = "criarconta.html"
-    form_class = CriarContaForm
+    template_name = "criarconta.html" #
+    form_class = CriarContaForm #
 
     def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+        form.save() #
+        return super().form_valid(form) #
 
     def get_success_url(self):
-        return reverse('narrativa:login')
+        return reverse('narrativa:login') #
 
 
 class PacienteNarrativas(ListView):
-    template_name = "paciente_narrativas.html"
-    model = Narrativa
+    template_name = "paciente_narrativas.html" #
+    model = Narrativa #
 
 
 class PacienteDetalhes(DetailView):
-    template_name = "paciente_detalhes.html"
-    model = Narrativa
+    template_name = "paciente_detalhes.html" #
+    model = Narrativa #
 
     def get_context_data(self, **kwargs):
-        context = super(PacienteDetalhes, self).get_context_data(**kwargs)
-        relacionados = Narrativa.objects.filter(categoria=self.get_object().categoria).order_by('-visualizacoes')[0:5]
-        context["relacionados"] = relacionados
-        return context
+        context = super(PacienteDetalhes, self).get_context_data(**kwargs) #
+        relacionados = Narrativa.objects.filter(categoria=self.get_object().categoria).order_by('-visualizacoes')[0:5] #
+        context["relacionados"] = relacionados #
+        return context #
 
 
 def iniciar_jornada_paciente(request, narrativa_id):
-    narrativa = get_object_or_404(Narrativa, pk=narrativa_id)
+    narrativa = get_object_or_404(Narrativa, pk=narrativa_id) #
 
-    if not request.session.session_key:
-        request.session.create()
-    session_key = request.session.session_key
+    if not request.session.session_key: #
+        request.session.create() #
+    session_key = request.session.session_key #
 
-    SessaoPaciente.objects.get_or_create(
-        session_key=session_key,
-        defaults={'narrativa_perfil': narrativa}
+    SessaoPaciente.objects.get_or_create( #
+        session_key=session_key, #
+        defaults={'narrativa_perfil': narrativa} #
     )
 
-    if not narrativa.cena_inicial:
-        messages.warning(request, f"A jornada '{narrativa.titulo}' ainda não está pronta para ser iniciada.")
-        return redirect('narrativa:paciente_narrativas')
+    if not narrativa.cena_inicial: #
+        messages.warning(request, f"A jornada '{narrativa.titulo}' ainda não está pronta para ser iniciada.") #
+        return redirect('narrativa:paciente_narrativas') #
 
-    return redirect('narrativa:exibir_cena_paciente', cena_id=narrativa.cena_inicial.id)
+    return redirect('narrativa:exibir_cena_paciente', cena_id=narrativa.cena_inicial.id) #
 
 
 def exibir_cena_paciente(request, cena_id):
-    cena = get_object_or_404(Cena, pk=cena_id)
+    cena = get_object_or_404(Cena, pk=cena_id) #
     try:
-        todas_cenas = list(cena.narrativa.cenas.all().order_by('id'))
-        total_cenas = len(todas_cenas)
-        progresso_atual = todas_cenas.index(cena) + 1
-        percentual = int((progresso_atual / total_cenas) * 100)
+        todas_cenas = list(cena.narrativa.cenas.all().order_by('id')) #
+        total_cenas = len(todas_cenas) #
+        progresso_atual = todas_cenas.index(cena) + 1 #
+        percentual = int((progresso_atual / total_cenas) * 100) #
     except (ValueError, ZeroDivisionError):
-        total_cenas = 0
-        progresso_atual = 0
-        percentual = 0
-    context = {
-        'cena': cena,
-        'total_cenas': total_cenas,
-        'progresso_atual': progresso_atual,
-        'percentual': percentual,
+        total_cenas = 0 #
+        progresso_atual = 0 #
+        percentual = 0 #
+    context = { #
+        'cena': cena, #
+        'total_cenas': total_cenas, #
+        'progresso_atual': progresso_atual, #
+        'percentual': percentual, #
     }
-    return render(request, 'cena_paciente.html', context)
+    return render(request, 'cena_paciente.html', context) #
 
 
 def responder_questionario(request, questionario_id):
-    questionario = get_object_or_404(Questionario, pk=questionario_id)
-    if request.method == "POST":
-        if not request.session.session_key:
-            request.session.create()
-        session_key = request.session.session_key
-        for pergunta in questionario.perguntas.all():
-            if pergunta.tipo_resposta == "MULTIPLA_ESCOLHA":
-                respostas = request.POST.getlist(f'pergunta_{pergunta.id}')
-                texto_resposta = ",".join(respostas)
+    questionario = get_object_or_404(Questionario, pk=questionario_id) #
+    if request.method == "POST": #
+        if not request.session.session_key: #
+            request.session.create() #
+        session_key = request.session.session_key #
+        for pergunta in questionario.perguntas.all(): #
+            if pergunta.tipo_resposta == "MULTIPLA_ESCOLHA": #
+                respostas = request.POST.getlist(f'pergunta_{pergunta.id}') #
+                texto_resposta = ",".join(respostas) #
             else:
-                texto_resposta = request.POST.get(f'pergunta_{pergunta.id}')
-            if texto_resposta:
-                Resposta.objects.create(
-                    pergunta=pergunta,
-                    session_key=session_key,
-                    texto_resposta=texto_resposta
+                texto_resposta = request.POST.get(f'pergunta_{pergunta.id}') #
+            if texto_resposta: #
+                Resposta.objects.create( #
+                    pergunta=pergunta, #
+                    session_key=session_key, #
+                    texto_resposta=texto_resposta #
                 )
 
-        # --- CORREÇÃO DE LÓGICA APLICADA AQUI ---
-        # Se houver uma cena de destino, vá para ela.
-        # Se não, volte para a lista de narrativas.
-        if questionario.cena_destino:
-            return redirect('narrativa:exibir_cena_paciente', cena_id=questionario.cena_destino.id)
+        if questionario.cena_destino: #
+            return redirect('narrativa:exibir_cena_paciente', cena_id=questionario.cena_destino.id) #
         else:
-            # Se não houver destino, talvez voltar para a cena de onde veio
-            # ou para a home de narrativas. A home parece mais segura.
-            if questionario.cena_associada:
-                 return redirect('narrativa:exibir_cena_paciente', cena_id=questionario.cena_associada.id)
-            return redirect('narrativa:paciente_narrativas')
+            if questionario.cena_associada: #
+                 return redirect('narrativa:exibir_cena_paciente', cena_id=questionario.cena_associada.id) #
+            return redirect('narrativa:paciente_narrativas') #
 
-    context = {
-        'questionario': questionario
+    context = { #
+        'questionario': questionario #
     }
-    return render(request, 'questionario.html', context)
+    return render(request, 'questionario.html', context) #
