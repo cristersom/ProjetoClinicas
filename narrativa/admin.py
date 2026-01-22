8857cd0f32fff32321e67a38976b26214583833a
cristersom <cristersom@gmail.com>
2026-01-22 10:57:28 -0300

    Limpeza de dados e reforço de isolamento SaaS

--- narrativa/admin.py
+++ narrativa/admin.py
@@ -1,74 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-import nested_admin
-from .models import (
-    Narrativa, Cena, Escolha, Questionario, Pergunta, Usuario, Resposta,
-    SessaoPaciente, OpcaoResposta, LogVisitaCena,
-    Categoria, Clinica  # Substituído: ConfiguracaoClinica por Clinica
-)
-from import_export.admin import ImportExportModelAdmin
-from import_export import resources
-from django.urls import path, reverse
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.utils.html import format_html
-import json
-
-from .forms import PerguntaAdminForm
-
-# --- GESTÃO SAAS: CLÍNICAS ---
-@admin.register(Clinica)
-class ClinicaAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'slug', 'assinatura_ativa', 'data_expiracao')
-    prepopulated_fields = {"slug": ("nome",)}
-    search_fields = ('nome',)
-
-@admin.register(Categoria)
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'clinica')
-    list_filter = ('clinica',)
-
-class EscolhaInline(admin.TabularInline):
-    model = Escolha
-    fk_name = 'cena_origem'
-    extra = 1
-
-class OpcaoRespostaInline(nested_admin.NestedTabularInline):
-    model = OpcaoResposta
-    extra = 0
-
-class PerguntaInline(nested_admin.NestedTabularInline):
-    model = Pergunta
-    form = PerguntaAdminForm
-    extra = 1
-    inlines = [OpcaoRespostaInline]
-
-@admin.register(Cena)
-class CenaAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'narrativa')
-    list_filter = ('narrativa__clinica', 'narrativa')
-    inlines = [EscolhaInline]
+from .models import Narrativa, Cena, Clinica, Usuario, Categoria, Resposta, Questionario, LogVisitaCena, SessaoPaciente
 
 @admin.register(Narrativa)
 class NarrativaAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'clinica', 'categoria', 'visualizacoes')
-    list_filter = ('clinica', 'categoria')
+    list_display = ('titulo', 'clinica', 'visualizacoes')
+    list_filter = ('clinica',)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs if request.user.is_superuser else qs.filter(clinica=request.user.clinica)
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            obj.clinica = request.user.clinica
+        super().save_model(request, obj, form, change)
 
-@admin.register(Questionario)
-class QuestionarioAdmin(nested_admin.NestedModelAdmin):
-    list_display = ('titulo', 'cena_associada')
-    inlines = [PerguntaInline]
-    class Media:
-        css = {'all': ('css/admin_custom.css',)}
+@admin.register(Clinica)
+class ClinicaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'slug', 'assinatura_ativa')
+    prepopulated_fields = {"slug": ("nome",)}
+    def has_module_permission(self, request):
+        return request.user.is_superuser
 
 @admin.register(Usuario)
 class UsuarioSaaSAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Gestão SaaS', {'fields': ('clinica', 'is_admin_clinica')}),
-    )
+    fieldsets = UserAdmin.fieldsets + (('SaaS', {'fields': ('clinica', 'is_admin_clinica')}),)
     list_display = ('username', 'email', 'clinica', 'is_staff')
-    list_filter = ('clinica', 'is_staff')
 
-admin.site.register(SessaoPaciente)
-admin.site.register(Resposta)
-admin.site.register(LogVisitaCena)
+admin.site.register([Cena, Categoria, Questionario, Resposta, LogVisitaCena, SessaoPaciente])
--- narrativa/views.py
+++ narrativa/views.py
@@ -6,22 +6,22 @@ from django.views.generic import ListView, DetailView, FormView, UpdateView, Tem
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import admin
 from django.conf import settings
-from django.utils import timezone
 import json
 
-TERMOS_ACEITOS_KEY = getattr(settings, 'TERMOS_ACEITOS_KEY', 'termo_aceite_session')
 
-
-# --- MIXIN PARA ISOLAMENTO DE DADOS (SaaS) ---
+# --- MIXIN PARA ISOLAMENTO SAAS ---
 class ClinicaFilterMixin:
+    """Garante que o usuário veja apenas dados da sua clínica."""
+
     def get_queryset(self):
         queryset = super().get_queryset()
+        if self.request.user.is_superuser:
+            return queryset
         if self.request.user.is_authenticated and self.request.user.clinica:
             return queryset.filter(clinica=self.request.user.clinica)
         return queryset.none()
 
 
-# --- ÁREA PÚBLICA ---
 class Homepage(FormView):
     template_name = "homepage.html"
     form_class = FormHomepage
@@ -38,6 +38,48 @@ class Homepage(FormView):
         return reverse('narrativa:criarconta')
 
 
+class Narrativas(LoginRequiredMixin, ClinicaFilterMixin, ListView):
+    template_name = "narrativas.html"
+    model = Narrativa
+
+
+class Detalhes(LoginRequiredMixin, DetailView):
+    template_name = "detalhes.html"
+    model = Narrativa
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Narrativa.objects.all()
+        return Narrativa.objects.filter(clinica=self.request.user.clinica)
+
+
+class PacienteNarrativas(ListView):
+    template_name = "paciente_narrativas.html"
+    model = Narrativa
+
+    def get_queryset(self):
+        return Narrativa.objects.all().order_by('-visualizacoes')
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "admin/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(admin.site.each_context(self.request))
+        clinica = self.request.user.clinica
+        if clinica:
+            context['kpi_sessoes'] = SessaoPaciente.objects.filter(narrativa_perfil__clinica=clinica).count()
+            context['kpi_respostas'] = Resposta.objects.filter(
+                pergunta__questionario__cena_associada__narrativa__clinica=clinica).count()
+            context['kpi_narrativas'] = Narrativa.objects.filter(clinica=clinica).count()
+
+            top = Narrativa.objects.filter(clinica=clinica).order_by('-visualizacoes')[:5]
+            context['chart_top_labels'] = json.dumps([n.titulo for n in top])
+            context['chart_top_data'] = json.dumps([n.visualizacoes for n in top])
+        return context
+
+
 class Criarconta(FormView):
     template_name = "criarconta.html"
     form_class = CriarContaForm
@@ -50,26 +92,6 @@ class Criarconta(FormView):
         return reverse('narrativa:login')
 
 
-# --- ÁREA ADMINISTRATIVA (LOGADA) ---
-class Narrativas(LoginRequiredMixin, ClinicaFilterMixin, ListView):
-    template_name = "narrativas.html"
-    model = Narrativa
-
-
-class Detalhes(LoginRequiredMixin, DetailView):
-    template_name = "detalhes.html"
-    model = Narrativa
-
-    def get_queryset(self):
-        return Narrativa.objects.filter(clinica=self.request.user.clinica)
-
-    def get(self, request, *args, **kwargs):
-        narrativa = self.get_object()
-        narrativa.visualizacoes += 1
-        narrativa.save()
-        return super().get(request, *args, **kwargs)
-
-
 class PerfilView(LoginRequiredMixin, UpdateView):
     template_name = "perfil.html"
     model = Usuario
@@ -79,98 +101,29 @@ class PerfilView(LoginRequiredMixin, UpdateView):
         return reverse('narrativa:narrativas')
 
 
-# --- ÁREA DO PACIENTE (JORNADA) ---
-class PacienteNarrativas(ListView):
-    template_name = "paciente_narrativas.html"
-    model = Narrativa
-
-    def get_queryset(self):
-        # Em um SaaS real, você filtraria pelo slug da clínica na URL
-        return Narrativa.objects.all().order_by('-visualizacoes')
-
-
-class PacienteDetalhes(DetailView):
-    template_name = "paciente_detalhes.html"
-    model = Narrativa
-
-    def post(self, request, *args, **kwargs):
-        narrativa = self.get_object()
-        if not request.session.session_key:
-            request.session.create()
-
-        sessao, created = SessaoPaciente.objects.get_or_create(session_key=request.session.session_key)
-        sessao.narrativa_perfil = narrativa
-        sessao.save()
-
-        if narrativa.cena_inicial:
-            return redirect('narrativa:exibir_cena_paciente', cena_id=narrativa.cena_inicial.id)
-        return redirect('narrativa:paciente_narrativas')
-
-
 def exibir_cena_paciente(request, cena_id):
     cena = get_object_or_404(Cena, pk=cena_id)
-    if not request.session.session_key:
-        request.session.create()
-
-    LogVisitaCena.objects.get_or_create(
-        session_key=request.session.session_key,
-        cena_visitada=cena,
-        defaults={'narrativa_associada': cena.narrativa}
-    )
-
+    LogVisitaCena.objects.get_or_create(session_key=request.session.session_key, cena_visitada=cena,
+                                        defaults={'narrativa_associada': cena.narrativa})
     return render(request, 'cena_paciente.html', {'cena': cena})
 
 
 def responder_questionario(request, questionario_id):
     questionario = get_object_or_404(Questionario, pk=questionario_id)
     if request.method == "POST":
-        if not request.session.session_key:
-            request.session.create()
-
         for pergunta in questionario.perguntas.all():
             resp = request.POST.get(f'pergunta_{pergunta.id}')
             if resp:
                 Resposta.objects.create(pergunta=pergunta, session_key=request.session.session_key, texto_resposta=resp)
-
-        if questionario.cena_destino:
-            return redirect('narrativa:exibir_cena_paciente', cena_id=questionario.cena_destino.id)
-        return redirect('narrativa:paciente_narrativas')
-
+        return redirect('narrativa:exibir_cena_paciente',
+                        cena_id=questionario.cena_destino.id) if questionario.cena_destino else redirect(
+            'narrativa:paciente_narrativas')
     return render(request, 'questionario.html', {'questionario': questionario})
 
 
-# --- FEEDBACK E DASHBOARD ---
 def perfil_sessao_view(request, narrativa_id):
-    session_key = request.session.session_key
-    if not session_key: return redirect('narrativa:ler_termos')
-
     narrativa = get_object_or_404(Narrativa, pk=narrativa_id)
-    context = {
-        'narrativa': narrativa,
-        'nome_perfil': narrativa.titulo,
-        'cenas_visitadas': LogVisitaCena.objects.filter(session_key=session_key, narrativa_associada=narrativa).count(),
-        'total_cenas': cena.narrativa.cenas.count() if 'cena' in locals() else 10  # Fallback
-    }
-    return render(request, 'perfil_sessao.html', context)
-
-
-class DashboardView(LoginRequiredMixin, TemplateView):
-    template_name = "admin/dashboard.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update(admin.site.each_context(self.request))
-        clinica = self.request.user.clinica
-
-        context['kpi_sessoes'] = SessaoPaciente.objects.filter(narrativa_perfil__clinica=clinica).count()
-        context['kpi_respostas'] = Resposta.objects.filter(
-            pergunta__questionario__cena_associada__narrativa__clinica=clinica).count()
-        context['kpi_narrativas'] = Narrativa.objects.filter(clinica=clinica).count()
-
-        top = Narrativa.objects.filter(clinica=clinica).order_by('-visualizacoes')[:5]
-        context['chart_top_labels'] = json.dumps([n.titulo for n in top])
-        context['chart_top_data'] = json.dumps([n.visualizacoes for n in top])
-        return context
+    return render(request, 'perfil_sessao.html', {'narrativa': narrativa})
 
 
 class TermosView(TemplateView): template_name = "termos.html"
@@ -183,5 +136,5 @@ class LerTermosView(TemplateView):
     template_name = "ler_termos.html"
 
     def post(self, request, *args, **kwargs):
-        request.session[TERMOS_ACEITOS_KEY] = True
+        request.session[getattr(settings, 'TERMOS_ACEITOS_KEY', 'termo_aceite_session')] = True
         return redirect('narrativa:paciente_narrativas')
