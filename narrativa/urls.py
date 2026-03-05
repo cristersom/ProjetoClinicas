4ad096db64bdee36ca3da76299848ad0fdf6ec73
cristersom <cristersom@gmail.com>
2026-03-05 13:22:45 -0300

    Fix: nome da rota de checkout no template de planos

--- narrativa/urls.py
+++ narrativa/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('planos/', views.PlanosView.as_view(), name='planos'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('criarconta/', views.LoginView.as_view(), name='criarconta'), # Adicione esta linha!
     path('criar-checkout-sessao/<str:price_id>/', views.criar_checkout_sessao, name='criar_checkout_sessao'),
+    path('criarconta/', views.LoginView.as_view(), name='criarconta'),  # Importante para o erro anterior
     path('sucesso/', views.sucesso_pagamento, name='sucesso_pagamento'),
 ]
