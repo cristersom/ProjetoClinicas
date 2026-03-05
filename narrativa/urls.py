c6cd965e2abd046dba89d5152acc2deed292834a
cristersom <cristersom@gmail.com>
2026-03-05 13:33:37 -0300

    Fix total: visual restaurado, erros de url limpos e stripe ativo

--- narrativa/urls.py
+++ narrativa/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('criar-checkout-sessao/<str:price_id>/', views.criar_checkout_sessao, name='criar_checkout_sessao'),
     path('criarconta/', views.LoginView.as_view(), name='criarconta'),
     path('sucesso/', views.sucesso_pagamento, name='sucesso_pagamento'),
-    # Adicionei esta linha para resolver o erro do link na Navbar:
-    path('minhas-narrativas/', views.HomeView.as_view(), name='narrativas'),
+    # Rotas de segurança para evitar erro na Navbar
+    path('minhas-jornadas/', views.HomeView.as_view(), name='narrativas'),
+    path('dashboard/', views.HomeView.as_view(), name='dashboard_admin'),
 ]
